ba020afb31cc6fbc907a40556e7b4248e1b29c0c
priyanshu lohar <priyanshulohar1@gmail.com>
2025-02-08 01:40:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createClient } = require("@supabase/supabase-js");
+const { createClient } = require("@supabase");
 const app = express();
 const cors = require('cors');
 app.use(express.json());
